f6f4ddfac9226942e17cd2364475d8aa0eab3159
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2021-10-24 06:24:34 -0600

    Update

--- lambda/utils/config.js
+++ lambda/utils/config.js
@@ -1,7 +1,5 @@
 export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8888'
-    : 'https://svelte-oauth-example.netlify.app';
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8888' : process.env.URL;
 export const ENDPOINT = process.env.NODE_ENV === 'development' ? '/.netlify/functions' : '/api';
 
 export const COOKIE_SECURE = process.env.NODE_ENV !== 'development';
