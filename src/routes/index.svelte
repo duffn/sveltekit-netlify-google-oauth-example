c7a318bbc6d2552e7ed68202170858b66b7084a6
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2021-10-23 20:49:44 -0600

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,38 +1,8 @@
 <script context="module">
-  export async function load({ page }) {
+  export async function load() {
     return {
-      headers: { Location: '/api/auth/google' },
+      redirect: '/api/auth/google',
       status: 302
     };
   }
 </script>
-
-<!-- <script>
-  import { onMount } from 'svelte';
-
-  let user;
-
-  onMount(async () => {
-    let endpoint = process.env.NODE_ENV === 'development' ? '/.netlify/functions' : '/api';
-    const res = await fetch(`${endpoint}/auth/status`);
-
-    if (res.ok) {
-      const json = await res.json();
-      user = json.email;
-    } else {
-      user = null;
-    }
-  });
-</script> -->
-
-<svelte:head>
-  <title>OAuth with Google</title>
-</svelte:head>
-
-<!-- {#if user}
-  LOGGED IN {user}
-{:else}
-  <a href="/api/auth/google">YOU NEED TO LOG IN</a>
-{/if} -->
-
-Hi
