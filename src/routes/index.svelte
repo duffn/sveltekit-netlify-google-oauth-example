e73177517ccf3e27c1cea071144104a59dda78ee
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2021-10-23 20:19:34 -0600

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,12 +6,12 @@
   onMount(async () => {
     let endpoint = process.env.NODE_ENV === 'development' ? '/.netlify/functions' : '/api';
     const res = await fetch(`${endpoint}/auth/status`);
-    const json = await res.json();
 
     if (res.ok) {
+      const json = await res.json();
       user = json.email;
     } else {
-      throw new Error(json);
+      user = null;
     }
   });
 </script>
