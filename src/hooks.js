087ab2269cc9f1e19b50209df0585e9a61088109
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2021-10-24 16:10:41 -0600

    Update

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,5 @@
 import cookie from 'cookie';
+import cookieSignature from 'cookie-signature';
 
 export async function handle({ request, resolve }) {
 	const cookies = cookie.parse(request.headers.cookie || '');
@@ -7,15 +8,25 @@ export async function handle({ request, resolve }) {
 
 	const response = await resolve(request);
 
-	response.headers['set-cookie'] = `user=${
-		request.locals.user || ''
-	}; Path=/; HttpOnly; SameSite=Lax;${process.env.NODE_ENV === 'production' ? ' Secure;' : ''}`;
+	let user = '';
+	if (request.locals.user) {
+		user = cookieSignature.sign(request.locals.user, process.env['COOKIE_SECRET']);
+	}
+
+	response.headers['set-cookie'] = `user=${user}; Path=/; HttpOnly; SameSite=Lax;${
+		process.env.NODE_ENV === 'production' ? ' Secure;' : ''
+	}`;
 
 	return response;
 }
 
 export async function getSession(request) {
+	let user = '';
+	if (request.locals.user) {
+		user = cookieSignature.unsign(request.locals.user, process.env['COOKIE_SECRET']);
+	}
+
 	return {
-		user: request.locals.user
+		user
 	};
 }
